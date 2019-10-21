25719668944b77c6c9444df68cbaa881c482b390
yangjun <290968698@qq.com>
2019-10-21 23:20:36 +0800

    add api of Ypool

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yangjuncode/ypool
 
 go 1.13
+
+require go.uber.org/atomic v1.4.0
--- ypool.go
+++ ypool.go
@@ -1,18 +1,49 @@
 package ypool
 
+import "go.uber.org/atomic"
+
 type Ypool struct {
 	//max go routine count for the pool
-	maxGoroutine int
+	maxPoolSize atomic.Uint32
+
+	//current goroutine size
+	currentPoolSize atomic.Uint32
+
+	//current running  goroutine size
+	runningGoroutineSize atomic.Uint32
 }
 
-//adjust pool size min size is 4, return old pool size
-func (this *Ypool) AdjustPoll(maxGoroutineCount int) int {
-	old := this.maxGoroutine
-	if maxGoroutineCount < 4 {
-		this.maxGoroutine = 4
+//adjust pool max size min size is 4, return old pool  max size
+func (this *Ypool) SetPoolMaxSize(newPoolMaxSize uint32) uint32 {
+	old := this.maxPoolSize.Load()
+	if newPoolMaxSize < 4 {
+		this.maxPoolSize.Store(4)
 	} else {
-		this.maxGoroutine = maxGoroutineCount
+		this.maxPoolSize.Store(newPoolMaxSize)
 	}
 
 	return old
+}
+
+// get pool max size
+func (this *Ypool) GetPoolMaxSize() uint32 {
+	return this.maxPoolSize.Load()
+}
+
+func (this *Ypool) Submit(task func()) error {
+	//todo impl submit
+	return nil
+}
+func (this *Ypool) SubmitPriority(priority int, task func()) error {
+	//todo impl SubmitQueue
+	return nil
+}
+
+func (this *Ypool) SubmitQueue(queueName interface{}, task func()) error {
+	//todo impl SubmitQueue
+	return nil
+}
+func (this *Ypool) SubmitQueuePriority(priority int, queueName interface{}, task func()) error {
+	//todo impl SubmitQueue
+	return nil
 }
