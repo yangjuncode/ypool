356767521c8ed980a04cad70f7b931f2f2179cd8
yangjun <290968698@qq.com>
2019-10-22 10:16:23 +0800

    add priority type

--- ypool.go
+++ ypool.go
@@ -2,6 +2,7 @@ package ypool
 
 import "go.uber.org/atomic"
 
+//the pool manager
 type Ypool struct {
 	//max go routine count for the pool
 	maxPoolSize atomic.Uint32
@@ -12,6 +13,20 @@ type Ypool struct {
 	//current running  goroutine size
 	runningGoroutineSize atomic.Uint32
 }
+
+//task priority type with level: None=0,Normal=1,High=2,Emergent=3
+type TaskPriority int
+
+const (
+	//the lowest priority level,use for logging and no time requirement task
+	None TaskPriority = iota
+	//the normal priority for common use
+	Normal
+	//for high level use
+	High
+	//the highest priority
+	Emergent
+)
 
 //adjust pool max size min size is 4, return old pool  max size
 func (this *Ypool) SetPoolMaxSize(newPoolMaxSize uint32) uint32 {
@@ -30,20 +45,26 @@ func (this *Ypool) GetPoolMaxSize() uint32 {
 	return this.maxPoolSize.Load()
 }
 
+//submit a task in pool, equal to SubmitPriority(Low,task)
 func (this *Ypool) Submit(task func()) error {
 	//todo impl submit
 	return nil
 }
-func (this *Ypool) SubmitPriority(priority int, task func()) error {
+
+//submit a task with priority
+func (this *Ypool) SubmitPriority(priority TaskPriority, task func()) error {
 	//todo impl SubmitQueue
 	return nil
 }
 
+//submit a task to pool in queue with name:queueName, equal to SubmitQueuePriority(Low,queueName,task)
 func (this *Ypool) SubmitQueue(queueName interface{}, task func()) error {
 	//todo impl SubmitQueue
 	return nil
 }
-func (this *Ypool) SubmitQueuePriority(priority int, queueName interface{}, task func()) error {
+
+//submit a task to pool in queue with priority and queueName
+func (this *Ypool) SubmitQueuePriority(priority TaskPriority, queueName interface{}, task func()) error {
 	//todo impl SubmitQueue
 	return nil
 }
